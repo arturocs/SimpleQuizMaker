703e72f0739474fd721f15e118df896bf9bca330
Arturo <arturocs@protonmail.com>
2025-03-13 16:40:43 +0100

    Add build

---
+++ build/_app/immutable/chunks/DABm8j4z.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_18j4nf9)==null?void 0:s.base)??"/SimpleQuizMaker";var e;const t=((e=globalThis.__sveltekit_18j4nf9)==null?void 0:e.assets)??a;export{t as a,a as b};
---
+++ build/_app/immutable/entry/start.BFBwQptE.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DEf_pQfA.js";export{o as load_css,r as start};
