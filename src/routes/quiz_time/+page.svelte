ddfe96d0b0285c01f913715dc127c185eb3ec6b4
Arturo <arturocs@protonmail.com>
2025-03-13 15:41:26 +0100

    Replace svg with bootstrap icon

--- src/routes/quiz_time/+page.svelte
+++ src/routes/quiz_time/+page.svelte
@@ -5,7 +5,8 @@
     import Quiz from "$lib/Quiz.svelte";
     import CheckSquareFill from "svelte-bootstrap-icons/lib/CheckSquareFill.svelte";
     import XSquareFill from "svelte-bootstrap-icons/lib/XSquareFill.svelte";
-    import { base } from '$app/paths';
+    import ExclamationTriangle from "svelte-bootstrap-icons/lib/ExclamationTriangle.svelte";
+    import { base } from "$app/paths";
     // Estado para almacenar las preguntas
     let questions: LocalStore<QuestionData[]> = $state(
         localStore("questions", []),
@@ -129,22 +130,11 @@
     {#if questions.value.length === 0}
         <div class="alert alert-warning">
             <div>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="stroke-current flex-shrink-0 h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    ><path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                    /></svg
-                >
-                <span
-                    >No hay preguntas disponibles. Por favor, crea algunas
-                    preguntas primero.</span
-                >
+                <ExclamationTriangle />
+                <span>
+                    No hay preguntas disponibles. Por favor, crea algunas
+                    preguntas primero.
+                </span>
             </div>
             <div class="flex-none">
                 <a href="{base}/" class="btn btn-sm">Volver al inicio</a>
