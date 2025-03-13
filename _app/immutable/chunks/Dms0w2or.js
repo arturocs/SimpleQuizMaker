e6a5792e3ecf86814946cf57af399d49b1aec779
Arturo <arturocs@protonmail.com>
2025-03-13 16:47:20 +0100

    Delete old build files

--- _app/immutable/chunks/Dms0w2or.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_19apscb)==null?void 0:s.base)??"/SimpleQuizMaker";var a;const t=((a=globalThis.__sveltekit_19apscb)==null?void 0:a.assets)??e;export{t as a,e as b};
